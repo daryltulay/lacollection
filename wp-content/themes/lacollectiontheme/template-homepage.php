b876bf4859f814ee4ba0b673f3b3554ba104ab9c
daryltulay <daryl.tulay@outlook.com>
2014-08-04 14:21:45 +0800

    Added imgs at home template

--- wp-content/themes/lacollectiontheme/template-homepage.php
+++ wp-content/themes/lacollectiontheme/template-homepage.php
@@ -11,11 +11,43 @@ Template Name: HomepageTemplate
 <?php endwhile; ?>
 -->
 <div class="container">
-	<div class="col-md-12">
-	<video autoplay loop poster="/assets/vid/polina.jpg" id="bgvid">
-	<!-- <source src="polina.webm" type="video/webm"> -->
-	<source src="<?php echo get_site_url(); ?>/assets/vid/videobackground.mp4" type="video/mp4">
-	</video>
+		<video autoplay loop poster="/assets/vid/polina.jpg" id="bgvid">
+		<!-- <source src="polina.webm" type="video/webm"> -->
+		<source src="<?php bloginfo('template_directory'); ?>/assets/vid/videobackground.mp4" type="video/mp4">
+		</video>
+		
+	<div class="col-md-offset-6 col-md-6">
+		
+		
+		<img src="<?php bloginfo('template_directory'); ?>/assets/img/logo-fm-group.png">
+		<img src="<?php bloginfo('template_directory'); ?>/assets/img/logo-federico.png">
+		<h3> By Appointments Only </h3>
+		<img src="<?php bloginfo('template_directory'); ?>/assets/img/line2.png">
+		
+		<h4> The Most Exclusive FM Perfume Collection in the World </h4>
+		<span> Made in Europe </span>
+		
+		
+		<div class="col-lg-2 bgc-sidebarmenu" style=" margin-top: 50px; width: 187px;height:300px;">
+	<div class="side-nav">
+		<nav class="s-mfix nav nav-stacked" role="navigation">
+	      <?php
+	        if (has_nav_menu('primary_navigation')) :
+	          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 's-menu list-unstyled'));
+	        endif;
+	      ?>
+	</nav>
+	</div>
+	
+	<nav class="s-mfix nav nav-stacked" role="navigation">
+	      <?php
+	        if (has_nav_menu('side_navigation')) :
+	          wp_nav_menu(array('theme_location' => 'side_navigation', 'menu_class' => 's-border text10 list-unstyled'));
+	        endif;
+	      ?>
+	</nav>
+</div>
+
 	</div>
 
 </div>
